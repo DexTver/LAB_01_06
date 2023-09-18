6d5f339630a0cb48b0dbbf5ce39e58f6b3286984
User <ivan.sharapov.05@yandex.ru>
2023-09-18 12:42:13 +0300

    Initial commit

---
+++ sharapov_lab01.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+    int x, s = 0;
+    printf("Write your digit:");
+    scanf("%i", &x);
+    for (int i = 1; i <= x / 2; ++i) {
+        if (x % i == 0) {
+            s += i;
+//            printf("%i\n", i);
+        }
+    }
+    if (x > s) {
+        printf("Yes!");
+    } else {
+        printf("No!");
+    }
+    return 0;
+}
